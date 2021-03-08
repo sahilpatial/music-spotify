bc8be307e5072482de50084ecd501e48b091033c
Sahil Patial <sahilpatial2001@gmail.com>
2021-03-08 10:43:23 +0530

    commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+  
+  return <img 
+  className="sidebar__logo"
+    src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png" alt="spotify" />;
+};
+
+export default Header;
